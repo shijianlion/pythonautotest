0d60455ed0bf2c2a5d382eba73f8f833e7345e13
shijianlion <549502961@qq.com>
2019-06-24 22:15:05 +0800

    提交修改

---
+++ sjappiumframework/business/login.py
@@ -0,0 +1,18 @@
+'''
+Created on 2019年3月18日
+
+@author: 博为峰
+'''
+from pageobject.elements import *
+from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+def login(self,username,password,expected):
+    dr = self.driver
+    dr.find_element(*username_loc).clear()
+    dr.find_element(*username_loc).send_keys(username)
+    dr.find_element(*password_loc).clear()
+    dr.find_element(*password_loc).send_keys(password)
+    dr.find_element(*btn_login_loc).click()
+    WebDriverWait(dr, 20, 0.5).until(EC.presence_of_element_located(msg_login_success_loc)) 
+    actual = dr.find_element(*msg_login_success_loc).text
+    self.assertEqual(expected, actual,"使用正常的帐号登录，但是没有登录成功，测试用例FAIL")
