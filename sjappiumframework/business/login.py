5b50c8802be94a1c354dd97f01f3275f834adad2
shijianlion <549502961@qq.com>
2019-06-30 21:55:03 +0800

    frerere

--- sjappiumframework/business/login.py
+++ sjappiumframework/business/login.py
@@ -6,24 +6,9 @@ def login(self,username,password,expected):
     dr.find_element(*username_loc).clear()
     dr.find_element(*username_loc).send_keys(username)
     dr.find_element(*password_loc).clear()
-    dr.find_element(*password_loc).send_keys(password)
-    dr.find_element(*btn_login_loc).cl
-
-faefefefa44
-
-
-ggdgrdgsrgrg
-
-rgrgrgrgrg
-
-grgrgewgewgewg
-
-utfytyrty
-
-rragreht2223232323434
+    dr.find_element(*passwo
+ffefewfewf
+    awefefefe
 
 
-
-qfegf
-
-232434343
+    eafewfewffwee
